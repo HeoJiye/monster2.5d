dd656f32169dd2c72a5dffae2feaa99b61b84ec9
yslle <dltmddus9082@naver.com>
2022-02-04 21:29:55 +0900

    220204

--- Assets/Common/Scripts/BasicCharacter.cs
+++ Assets/Common/Scripts/BasicCharacter.cs
@@ -1,78 +1,94 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Common.Scripts
 {
-  public class BasicCharacter : MonoBehaviour
-  {
-    #region Constants
-
-    private static readonly int WALK_PROPERTY = Animator.StringToHash("Walk");
-
-    #endregion
-
-
-    #region Inspector
-
-    [SerializeField]
-    private float speed = 2f;
-
-    [Header("Relations")]
-    [SerializeField]
-    private Animator animator = null;
-
-    [SerializeField]
-    private Rigidbody physicsBody = null;
-
-    [SerializeField]
-    private SpriteRenderer spriteRenderer = null;
-
-    #endregion
-
-
-    #region Fields
-
-    private Vector3 _movement;
-
-    #endregion
-
-
-    #region MonoBehaviour
-
-    private void Update ()
+    public class BasicCharacter : MonoBehaviour
     {
-      // Vertical
-      float inputY = 0;
-      if ( Input.GetKey(KeyCode.UpArrow) )
-        inputY = 1;
-      else if ( Input.GetKey(KeyCode.DownArrow) )
-        inputY = -1;
+        #region Constants
 
-      // Horizontal
-      float inputX = 0;
-      if ( Input.GetKey(KeyCode.RightArrow) )
-      {
-        inputX = 1;
-        spriteRenderer.flipX = false;
-      }
-      else if ( Input.GetKey(KeyCode.LeftArrow) )
-      {
-        inputX = -1;
-        spriteRenderer.flipX = true;
-      }
+        private static readonly int WALK_PROPERTY = Animator.StringToHash("Walk");
 
-      // Normalize
-      _movement = new Vector3(inputX, 0, inputY).normalized;
+        #endregion
 
-      animator.SetBool(WALK_PROPERTY,
-                       Math.Abs(_movement.sqrMagnitude) > Mathf.Epsilon);
+
+        #region Inspector
+
+        [SerializeField]
+        private float speed = 2f;
+
+        [Header("Relations")]
+        [SerializeField]
+        private Animator animator = null;
+
+        [SerializeField]
+        private Rigidbody physicsBody = null;
+
+        [SerializeField]
+        private SpriteRenderer spriteRenderer = null;
+
+        #endregion
+
+
+        #region Fields
+
+        private Vector3 _movement;
+
+        #endregion
+        public bool isAttacked = false;
+        public int direction = 0;
+
+        void Start()
+        {
+            Invoke("Think", 3);
+        }
+
+        void Think()
+        {
+            if (!isAttacked)
+                direction = Random.Range(-2, 3);
+
+            float nextThinkTime = Random.Range(2f, 5f);
+            Invoke("Think", nextThinkTime);
+        }
+
+        #region MonoBehaviour
+
+        private void Update()
+        {
+            // Vertical
+            float inputY = 0;
+            if (direction == -1)
+                inputY = 1;
+            else if (direction == 1)
+                inputY = -1;
+
+            // Horizontal
+            float inputX = 0;
+            if (direction == -2)
+            {
+                inputX = 1;
+                spriteRenderer.flipX = true;
+            }
+            else if (direction == 2)
+            {
+                inputX = -1;
+                spriteRenderer.flipX = false;
+            }
+
+            // Normalize
+            _movement = new Vector3(inputX, 0, inputY).normalized;
+
+            animator.SetBool(WALK_PROPERTY,
+                             Math.Abs(_movement.sqrMagnitude) > Mathf.Epsilon);
+        }
+
+        private void FixedUpdate()
+        {
+            physicsBody.velocity = _movement * speed;
+        }
+
+        #endregion
     }
-
-    private void FixedUpdate ()
-    {
-      physicsBody.velocity = _movement * speed;
-    }
-
-    #endregion
-  }
 }
